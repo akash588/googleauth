369582681acd2f69b3a44ffdf7e0570a964d12d1
Akash Shelar <79242986+akash588@users.noreply.github.com>
2022-06-07 11:48:37 +0530

    Delete database.js

--- Security/config/database.js
+++
@@ -1,8 +0,0 @@
-// config/database.js
-module.exports = {
-
-    
-    
-    'url' : 'mongodb://0.0.0.0:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false' // database connection URL
-
-};
