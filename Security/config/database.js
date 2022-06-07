274e0a658bfdf64153f067442bccdb3b2c2b086d
Akash Shelar <79242986+akash588@users.noreply.github.com>
2022-06-07 11:36:24 +0530

    db chnages

---
+++ Security/config/database.js
@@ -0,0 +1,8 @@
+// config/database.js
+module.exports = {
+
+    
+    
+    'url' : 'mongodb://0.0.0.0:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false' // database connection URL
+
+};
