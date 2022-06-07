74f257179df7c54631d127f3c770e65874a3a1aa
Akash Shelar <79242986+akash588@users.noreply.github.com>
2022-06-07 11:48:54 +0530

    db

---
+++ Security/config/database.js
@@ -0,0 +1,8 @@
+// config/database.js
+module.exports = {
+
+    
+    
+    'url' : 'mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false' // database connection URL
+
+};
